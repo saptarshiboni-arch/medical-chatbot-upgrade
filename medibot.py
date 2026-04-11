f3145d82598681f774df30ddd132e03354e5efc0
saptarshiboni-arch <saptarshiboni@gmail.com>
2026-04-12 00:23:01 +0530

    Update port and imports for render deployments

--- medibot.py
+++ medibot.py
@@ -5,6 +5,7 @@ from langchain_community.vectorstores import FAISS
 from langchain_core.prompts import PromptTemplate
 from langchain_groq import ChatGroq
 from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains import create_retrieval_chain
 
 from dotenv import load_dotenv
 load_dotenv()
